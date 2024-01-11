764e52c59fc4273f08b5c10c2c0b43504637ca52
aadibhat09 <aadibhat09@gmail.com>
2024-01-10 20:55:58 -0800

    variable name changes

--- Team-3749-2024/src/main/java/frc/robot/subsystems/swerve/real/SwerveModuleSparkMax.java
+++ Team-3749-2024/src/main/java/frc/robot/subsystems/swerve/real/SwerveModuleSparkMax.java
@@ -21,8 +21,8 @@ Very closely inspired by 6328's Swerve Sim code,
  https://github.com/Mechanical-Advantage/RobotCode2023/blob/main/src/main/java/org/littletonrobotics/frc2023/subsystems/drive/ModuleIOSim.java
 */
 public class SwerveModuleSparkMax implements SwerveModuleIO {
-    private CANSparkMax driveReal = new CANSparkMax(0, CANSparkMax.MotorType.kBrushless);
-    private CANSparkMax turnReal = new CANSparkMax(0, CANSparkMax.MotorType.kBrushless);
+    private CANSparkMax driveMotor = new CANSparkMax(0, CANSparkMax.MotorType.kBrushless);
+    private CANSparkMax turnMotor = new CANSparkMax(0, CANSparkMax.MotorType.kBrushless);
     private double turnPositionRad = 0;
     private double driveAppliedVolts = 0.0;
     private double turnAppliedVolts = 0.0;
@@ -35,8 +35,8 @@ public class SwerveModuleSparkMax implements SwerveModuleIO {
     public void updateData(ModuleData data) {
         // update sim values
         // how far have we turned in the previous loop?
-        double driveRadPerSec = Units.rotationsPerMinuteToRadiansPerSecond(driveReal.getEncoder().getVelocity()); 
-        double turnRadPerSec = Units.rotationsPerMinuteToRadiansPerSecond(turnReal.getEncoder().getVelocity());
+        double driveRadPerSec = Units.rotationsPerMinuteToRadiansPerSecond(driveMotor.getEncoder().getVelocity()); 
+        double turnRadPerSec = Units.rotationsPerMinuteToRadiansPerSecond(turnMotor.getEncoder().getVelocity());
         double angleDiffRad = turnRadPerSec * Sim.loopPeriodSec;
         
         // update our angle variables
@@ -53,13 +53,13 @@ public class SwerveModuleSparkMax implements SwerveModuleIO {
                 + (turnRadPerSec * 0.02 * ModuleConstants.wheelDiameterMeters) / 2;
         data.driveVelocityMPerSec = driveRadPerSec * ModuleConstants.wheelDiameterMeters / 2;
         data.driveAppliedVolts = driveAppliedVolts;
-        data.driveCurrentAmps = Math.abs(driveReal.getOutputCurrent());
+        data.driveCurrentAmps = Math.abs(driveMotor.getOutputCurrent());
         data.driveTempCelcius = 0;
 
         data.turnAbsolutePositionRad = turnPositionRad;
         data.turnVelocityRadPerSec = turnRadPerSec;
         data.turnAppliedVolts = turnAppliedVolts;
-        data.turnCurrentAmps = Math.abs(turnReal.getOutputCurrent());
+        data.turnCurrentAmps = Math.abs(turnMotor.getOutputCurrent());
         data.turnTempCelcius = 0;
 
 
@@ -67,11 +67,11 @@ public class SwerveModuleSparkMax implements SwerveModuleIO {
     @Override
     public void setDriveVoltage(double volts) {
         driveAppliedVolts = MathUtil.clamp(volts, -8.0, 8.0);
-        driveReal.setVoltage(driveAppliedVolts);
+        driveMotor.setVoltage(driveAppliedVolts);
     }
     @Override
     public void setTurnVoltage(double volts) {
         turnAppliedVolts = MathUtil.clamp(volts, -8.0, 8.0);
-        turnReal.setVoltage(turnAppliedVolts);
+        turnMotor.setVoltage(turnAppliedVolts);
     }
 }
