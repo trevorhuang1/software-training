0c4dece5025a616b1ba3e270ed67c98b0126e0df
spooketti <jonliu168@gmail.com>
2024-01-17 16:10:16 -0800

    fix the capital Shooter issue

--- Team-3749-2024/src/main/java/frc/robot/subsystems/wrist/WristSparkMax.java
+++ Team-3749-2024/src/main/java/frc/robot/subsystems/wrist/WristSparkMax.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.Constants;
+//a
 
 public class WristSparkMax extends SubsystemBase {
 
