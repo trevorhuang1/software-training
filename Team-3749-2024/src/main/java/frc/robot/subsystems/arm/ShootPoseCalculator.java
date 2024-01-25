625dc2892d2b752b60552b879ef513bbe001c5e7
Don Tran <donqt15@gmail.com>
2024-01-24 16:46:03 -0800

    shootposecalculator init

---
+++ Team-3749-2024/src/main/java/frc/robot/subsystems/arm/ShootPoseCalculator.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.arm;
+
+import edu.wpi.first.math.geometry.Pose2d;
+
+public class ShootPoseCalculator {
+    public static Pose2d ShootingPose2DCalculate(Pose2d currentPose2d){
+
+        
+        return null;
+    }
+
+    private static boolean isInRange(Pose2d currentPose2d){
+        
+
+        return false;
+    }
+}
