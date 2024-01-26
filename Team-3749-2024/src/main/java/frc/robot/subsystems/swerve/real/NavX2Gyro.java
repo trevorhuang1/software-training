70ad3e18c86ea2ca628113942998b9a7f883e6ea
NoahSimon8 <noah.simon9806@gmail.com>
2024-01-25 17:01:27 -0800

    made navx

--- Team-3749-2024/src/main/java/frc/robot/subsystems/swerve/real/NavX2Gyro.java
+++ Team-3749-2024/src/main/java/frc/robot/subsystems/swerve/real/NavX2Gyro.java
@@ -1,45 +1,43 @@
 package frc.robot.subsystems.swerve.real;
-// package frc.robot.subsystems.swerve;
 
-// import com.kauailabs.navx.frc.AHRS;
-// import edu.wpi.first.wpilibj.SPI;
-// import edu.wpi.first.wpilibj.interfaces.Gyro;
-
-
-// public class NavX2Gyro implements GyroIO{
-
-//     private final AHRS gyro = new AHRS(SPI.Port.kMXP);
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
+import frc.robot.subsystems.swerve.GyroIO;
 
 
-//     public NavX2Gyro() {
-//         gyro.calibrate();
-//         new Thread(() -> {
-//             try {
-//                 while (gyro.isCalibrating()){
+public class NavX2Gyro implements GyroIO{
+
+    private final AHRS gyro = new AHRS(SPI.Port.kMXP);
+    private final GyroData data = new GyroData();
+
+    public NavX2Gyro() {
+        new Thread(() -> {
+            try {
+                while (gyro.isCalibrating()){
                     
-//                 }
-//                 gyro.reset();
+                }
+                gyro.reset();
 
-//             } catch (Exception e) {
-//             }
-//         }).start();
-//     }
+            } catch (Exception e) {
+            }
+        }).start();
+    }
     
 
-//     @Override
-//     public void updateData(GyroData data){
-//         data.yaw = gyro.getYaw();
-//         data.pitch = gyro.getPitch();
-//         data.roll = gyro.getRoll();
-//         data.isCalibrating = gyro.isCalibrating();
-//         data.connected = gyro.isConnected();
+    @Override
+    public void updateData(GyroData data){
+        data.yawDeg = gyro.getYaw();
+        data.pitchDeg = gyro.getPitch();
+        data.rollDeg = gyro.getRoll();
+        data.isCalibrating = gyro.isCalibrating();
+        data.connected = gyro.isConnected();
 
 
-//     }
-//     @Override
-//     public void resetGyro(){
-//         gyro.reset();
+    }
+    @Override
+    public void resetGyro(){
+        gyro.reset();
 
-//     }
+    }
     
-// }
+}
