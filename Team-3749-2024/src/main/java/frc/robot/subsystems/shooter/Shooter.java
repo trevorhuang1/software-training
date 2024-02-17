017bc9a69b4cebdcce39d5e5106a2663ba2f0361
spooketti <jonliu168@gmail.com>
2024-02-16 16:31:44 -0800

    jadon told me to add antiShooterRubbing

--- Team-3749-2024/src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ Team-3749-2024/src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -15,6 +15,9 @@ public class Shooter extends SubsystemBase {
     private PIDController shooterController = new PIDController(Constants.ShintakeConstants.shooterPID.kP, Constants.ShintakeConstants.shooterPID.kI, Constants.ShintakeConstants.shooterPID.kD);
     private SimpleMotorFeedforward shooterFF = new SimpleMotorFeedforward(0, 1);  
     private double shooterVelocity = 0;
+    private double armAngle = 0;
+    private double antiShooterRubbing = 0; //i dont actually know what angle the arm is at when it's resting 
+    //and the wheels are touching the body of the robot but thats what this represents
 
     public Shooter() 
         {
@@ -32,6 +35,10 @@ public class Shooter extends SubsystemBase {
 
    public void moveShooter()
    {
+    if(armAngle == antiShooterRubbing)
+    {
+        return; //the wheels might still have inertia from spinning but it shouldn't be a problem?
+    }
     shooterIO.setVoltage(
         shooterController.calculate(data.leftShooterVelocityRadPerSec,shooterVelocity) + shooterFF.calculate(shooterVelocity),
         shooterController.calculate(data.rightShooterVelocityRadPerSec,shooterVelocity) + shooterFF.calculate(shooterVelocity)
