2f9d714c5a5bb5fc9fe8dd24105114cf7020ea6c
SlushEE0 <101753528+SlushEE0@users.noreply.github.com>
2024-02-19 15:49:58 -0800

    feat: shooter sysid

--- Team-3749-2024/src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ Team-3749-2024/src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,57 +1,165 @@
 package frc.robot.subsystems.shooter;
 
+import static edu.wpi.first.units.MutableMeasure.mutable;
+import static edu.wpi.first.units.Units.*;
+
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.units.*;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.MutableMeasure;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.Robot;
 import frc.robot.subsystems.shooter.ShooterIO.ShooterData;
 import frc.robot.utils.Constants;
 
 public class Shooter extends SubsystemBase {
 
-    private ShooterIO shooterIO;
-    private ShooterData data = new ShooterData();
-    private PIDController shooterController = new PIDController(Constants.ShintakeConstants.shooterPID.kP,
-            Constants.ShintakeConstants.shooterPID.kI, Constants.ShintakeConstants.shooterPID.kD);
-    private SimpleMotorFeedforward shooterFF = new SimpleMotorFeedforward(0, 1);
-    private double shooterVelocity = 0;
-    private double armAngle = 0;
-    private double antiShooterRubbing = 0; //i dont actually know what angle the arm is at when it's resting 
-    //and the wheels are touching the body of the robot but thats what this represents
+  private ShooterIO shooterIO;
+  private ShooterData data = new ShooterData();
+  private PIDController shooterController = new PIDController(
+    Constants.ShintakeConstants.shooterPID.kP,
+    Constants.ShintakeConstants.shooterPID.kI,
+    Constants.ShintakeConstants.shooterPID.kD
+  );
+  private SimpleMotorFeedforward shooterFF = new SimpleMotorFeedforward(0, 1);
+  private double shooterVelocity = 0;
+  private double armAngle = 0;
+  private double antiShooterRubbing = 0; //i dont actually know what angle the arm is at when it's resting
 
-    public Shooter() {
-        shooterIO = new ShooterSparkMax();
-        if (Robot.isSimulation()) {
-            shooterIO = new ShooterSim();
-        }
+  //and the wheels are touching the body of the robot but thats what this represents
+
+  public Shooter() {
+    shooterIO = new ShooterSparkMax();
+    if (Robot.isSimulation()) {
+      shooterIO = new ShooterSim();
     }
+  }
 
-    public void setShooterVelocity(double velocity) {
-        this.shooterVelocity = velocity;
+  public void setShooterVelocity(double velocity) {
+    this.shooterVelocity = velocity;
+  }
+
+  public void moveShooter() {
+    if (armAngle == antiShooterRubbing) {
+      return; //the wheels might still have inertia from spinning but it shouldn't be a problem?
     }
+    double voltage =
+      shooterController.calculate(
+        data.leftShooterVelocityRadPerSec,
+        shooterVelocity
+      ) +
+      shooterFF.calculate(shooterVelocity);
 
-    public void moveShooter() {
-       if(armAngle == antiShooterRubbing)
-    {
-        return; //the wheels might still have inertia from spinning but it shouldn't be a problem?
-    }
-     double voltage = shooterController.calculate(data.leftShooterVelocityRadPerSec, shooterVelocity)
-                + shooterFF.calculate(shooterVelocity);
+    setVoltage(voltage);
+  }
 
-        setVoltage(voltage);
-    }
+  public void setVoltage(double volts) {
+    shooterIO.setVoltage(volts, volts);
+  }
 
-    public void setVoltage(double volts) {
-        shooterIO.setVoltage(volts, volts);
-    }
+  @Override
+  public void periodic() {
+    shooterIO.updateData(data);
+    SmartDashboard.putNumber(
+      "shooterVelocity",
+      data.leftShooterVelocityRadPerSec
+    );
+    SmartDashboard.putNumber("shooterVolts", data.leftShooterVolts);
+    SmartDashboard.putNumber("shooterTemp", data.leftShooterTempCelcius);
 
-    @Override
-    public void periodic() {
-        shooterIO.updateData(data);
-        SmartDashboard.putNumber("shooterVelocity", data.leftShooterVelocityRadPerSec);
-        SmartDashboard.putNumber("shooterVolts", data.leftShooterVolts);
-        SmartDashboard.putNumber("shooterTemp", data.leftShooterTempCelcius);
-    }
+    leftShooterAbsPos += data.leftShooterVelocityRadPerSec * 0.02;
+    rightShooterAbsPos += data.rightShooterVelocityRadPerSec * 0.02;
+  }
 
+  private final MutableMeasure<Voltage> identificationTurnVoltageMeasure = mutable(
+    Volts.of(0)
+  );
+  private final MutableMeasure<Angle> identificationTurnDistanceMeasure = mutable(
+    Radians.of(0)
+  );
+  private final MutableMeasure<Velocity<Angle>> identificaitonTurnVelocityMeasure = mutable(
+    RadiansPerSecond.of(0)
+  );
+  private static double leftShooterAbsPos = 0;
+  private static double rightShooterAbsPos = 0;
+
+  private SysIdRoutine shooterRoutine = new SysIdRoutine(
+    // new SysIdRoutine.Config(),
+    new SysIdRoutine.Config(
+      Volts.per(Seconds).of(1),
+      Volts.of(7),
+      Seconds.of(10)
+    ),
+    new SysIdRoutine.Mechanism(
+      (Measure<Voltage> volts) -> {
+        setVoltage(volts.magnitude());
+      },
+      log -> {
+        log
+          .motor("shooter-left-motor")
+          .voltage(
+            identificationTurnVoltageMeasure.mut_replace(
+              data.leftShooterVolts,
+              Volts
+            )
+          )
+          .angularPosition(
+            identificationTurnDistanceMeasure.mut_replace(
+              leftShooterAbsPos,
+              Radians
+            )
+          )
+          .angularVelocity(
+            identificaitonTurnVelocityMeasure.mut_replace(
+              data.leftShooterVelocityRadPerSec,
+              RadiansPerSecond
+            )
+          );
+
+        log
+          .motor("shooter-right-motor")
+          .voltage(
+            identificationTurnVoltageMeasure.mut_replace(
+              data.rightShooterVolts,
+              Volts
+            )
+          )
+          .angularPosition(
+            identificationTurnDistanceMeasure.mut_replace(
+              rightShooterAbsPos,
+              Radians
+            )
+          )
+          .angularVelocity(
+            identificaitonTurnVelocityMeasure.mut_replace(
+              data.rightShooterVelocityRadPerSec,
+              RadiansPerSecond
+            )
+          );
+      },
+      this
+    )
+  );
+
+  public Command getShooterSysIDQuasistaticForwardTest() {
+    return shooterRoutine.quasistatic(Direction.kForward);
+  }
+
+  public Command getShooterSysIDQuasistaticReverseTest() {
+    return shooterRoutine.quasistatic(Direction.kReverse);
+  }
+
+  public Command getShooterSysIDDynamicForwardTest() {
+    return shooterRoutine.dynamic(Direction.kForward);
+  }
+
+  public Command getShooterSysIDDynamicReverseTest() {
+    return shooterRoutine.dynamic(Direction.kReverse);
+  }
 }
--- Team-3749-2024/src/main/java/frc/robot/utils/JoystickIO.java
+++ Team-3749-2024/src/main/java/frc/robot/utils/JoystickIO.java
@@ -1,7 +1,5 @@
 package frc.robot.utils;
 
-import java.util.Map;
-
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
@@ -13,92 +11,98 @@ import frc.robot.Robot;
 import frc.robot.commands.IntakeCommand;
 import frc.robot.commands.ShooterCommand;
 import frc.robot.commands.WristCommand;
+import frc.robot.subsystems.shooter.Shooter;
+import java.util.Map;
 
 /**
  * Util class for button bindings
- * 
+ *
  * @author Rohin Sood
  */
 public class JoystickIO {
-    private static String[] lastJoystickNames = new String[] { "", "", "", "", "", "" };
 
-    private Xbox pilot; 
-    private Xbox operator;
+  private static String[] lastJoystickNames = new String[] {
+    "",
+    "",
+    "",
+    "",
+    "",
+    ""
+  };
 
-    public JoystickIO(Xbox pilot, Xbox operator) {
-        this.pilot = pilot;
-        this.operator = operator;
+  private Xbox pilot;
+  private Xbox operator;
+
+  public JoystickIO(Xbox pilot, Xbox operator) {
+    this.pilot = pilot;
+    this.operator = operator;
+  }
+
+  public static boolean didJoysticksChange() {
+    boolean joysticksChanged = false;
+    for (int port = 0; port < DriverStation.kJoystickPorts; port++) {
+      String name = DriverStation.getJoystickName(port);
+      if (!name.equals(lastJoystickNames[port])) {
+        joysticksChanged = true;
+        lastJoystickNames[port] = name;
+      }
+    }
+    return joysticksChanged;
+  }
+
+  /**
+   * Calls binding methods according to the joysticks connected
+   */
+  public void getButtonBindings() {
+    if (DriverStation.isJoystickConnected(1)) {
+      // if both xbox controllers are connected
+      pilotAndOperatorBindings();
+    } else if (DriverStation.isJoystickConnected(0)) {
+      // if only one xbox controller is connected
+      pilotBindings();
+    } else {
+      // if no joysticks are connected (ShuffleBoard buttons)
+      noJoystickBindings();
     }
 
-    public static boolean didJoysticksChange() {
-        boolean joysticksChanged = false;
-        for (int port = 0; port < DriverStation.kJoystickPorts; port++) {
-            String name = DriverStation.getJoystickName(port);
-            if (!name.equals(lastJoystickNames[port])) {
-                joysticksChanged = true;
-                lastJoystickNames[port] = name;
-            }
-        }
-        return joysticksChanged;
-    }
+    setDefaultCommands();
+  }
 
-    /**
-     * Calls binding methods according to the joysticks connected
-     */
-    public void getButtonBindings() {
+  /**
+   * If both controllers are plugged in (pi and op)
+   */
+  public void pilotAndOperatorBindings() {
+    pilotBindings();
+  }
 
-        if (DriverStation.isJoystickConnected(1)) {
-            // if both xbox controllers are connected
-            pilotAndOperatorBindings();
+  /**
+   * If only one controller is plugged in (pi)
+   */
+  public void pilotBindings() {
+    pilot.aWhileHeld(Robot.shooter.getShooterSysIDDynamicForwardTest());
+    pilot.bWhileHeld(Robot.shooter.getShooterSysIDDynamicReverseTest());
+    pilot.yWhileHeld(Robot.shooter.getShooterSysIDQuasistaticForwardTest());
+    pilot.xWhileHeld(Robot.shooter.getShooterSysIDQuasistaticReverseTest());
+  }
 
-        } else if (DriverStation.isJoystickConnected(0)) {
-            // if only one xbox controller is connected
-            pilotBindings();
+  /**
+   * If NO joysticks are plugged in (Buttons for commands are runnable in the
+   * "Controls" tab in ShuffleBoard)
+   */
+  public void noJoystickBindings() {
+    ShuffleboardTab controlsTab = Shuffleboard.getTab("Controls");
 
-        } else {
-            // if no joysticks are connected (ShuffleBoard buttons)
-            noJoystickBindings();
-        }
+    // Example
+    ShuffleboardLayout armCommands = controlsTab
+      .getLayout("Arm", BuiltInLayouts.kList)
+      .withSize(2, 2)
+      .withProperties(Map.of("Label position", "HIDDEN")); // hide labels for commands
+  }
 
-        setDefaultCommands();
-    }
-
-    /**
-     * If both controllers are plugged in (pi and op)
-     */
-    public void pilotAndOperatorBindings() {
-
-
-    }
-
-    /**
-     * If only one controller is plugged in (pi)
-     */
-    public void pilotBindings() {
-        
-    }
-
-    /**
-     * If NO joysticks are plugged in (Buttons for commands are runnable in the
-     * "Controls" tab in ShuffleBoard)
-     */
-    public void noJoystickBindings() {
-        ShuffleboardTab controlsTab = Shuffleboard.getTab("Controls");
-
-        // Example
-        ShuffleboardLayout armCommands = controlsTab
-                .getLayout("Arm", BuiltInLayouts.kList)
-                .withSize(2, 2)
-                .withProperties(Map.of("Label position", "HIDDEN")); // hide labels for commands
-
-
-    }
-
-    /**
-     * Sets the default commands
-     */
-    public void setDefaultCommands() {
-        
+  /**
+   * Sets the default commands
+   */
+  public void setDefaultCommands() {
     //    Robot.wrist.setDefaultCommand(new WristCommand());
     //    Robot.intake.setDefaultCommand(new IntakeCommand());
     //    Robot.shooter.setDefaultCommand(new ShooterCommand());
@@ -113,15 +117,28 @@ public class JoystickIO {
     //    pilot.rightBumper().onFalse(Commands.runOnce(() -> Robot.shooter.setShooterVelocity(0)));
 
     //    pilot.a().onTrue(Commands.runOnce(() -> Robot.wrist.toggleWristGoal()));
-        
-        // Basic Voltage Testing
-        pilot.aWhileHeld(Commands.run(() -> Robot.wrist.setVoltage(7)), Commands.run(() -> Robot.wrist.setVoltage(0)));
-        pilot.bWhileHeld(Commands.run(() -> Robot.wrist.setVoltage(-7)), Commands.run(() -> Robot.wrist.setVoltage(0)));
 
-        pilot.xWhileHeld(Commands.run(() -> Robot.shooter.setVoltage(12)), Commands.run(() -> Robot.shooter.setVoltage(0)));
-        pilot.rightTriggerWhileHeld(Commands.run(() -> Robot.intake.setVoltage(-7)), Commands.run(() -> Robot.intake.setVoltage(0)));
-        pilot.leftTriggerWhileHeld(Commands.run(() -> Robot.intake.setVoltage(7)), Commands.run(() -> Robot.intake.setVoltage(0)));
+    // Basic Voltage Testing
+    pilot.aWhileHeld(
+      Commands.run(() -> Robot.wrist.setVoltage(7)),
+      Commands.run(() -> Robot.wrist.setVoltage(0))
+    );
+    pilot.bWhileHeld(
+      Commands.run(() -> Robot.wrist.setVoltage(-7)),
+      Commands.run(() -> Robot.wrist.setVoltage(0))
+    );
 
-    }
-        
+    pilot.xWhileHeld(
+      Commands.run(() -> Robot.shooter.setVoltage(12)),
+      Commands.run(() -> Robot.shooter.setVoltage(0))
+    );
+    pilot.rightTriggerWhileHeld(
+      Commands.run(() -> Robot.intake.setVoltage(-7)),
+      Commands.run(() -> Robot.intake.setVoltage(0))
+    );
+    pilot.leftTriggerWhileHeld(
+      Commands.run(() -> Robot.intake.setVoltage(7)),
+      Commands.run(() -> Robot.intake.setVoltage(0))
+    );
+  }
 }
