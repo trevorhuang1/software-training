c429e6f4b913a743192ca8e7ca578bd6ea35838c
aadibhat09 <aadibhat09@gmail.com>
2024-02-19 16:56:16 -0800

    intake sysid

--- Team-3749-2024/src/main/java/frc/robot/subsystems/intake/Intake.java
+++ Team-3749-2024/src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -7,6 +7,7 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.units.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.Robot;
@@ -48,28 +49,67 @@ public class Intake extends SubsystemBase {
     @Override
     public void periodic() {
         intakeIO.updateData(data);
+        
         SmartDashboard.putNumber("intakeVolts",data.intakeVolts);
         SmartDashboard.putNumber("intakeVelocityRadPerSec", data.intakeVelocityRadPerSec);
         SmartDashboard.putNumber("intakeTemp", data.intakeTempCelcius);
         SmartDashboard.putBoolean("intakeHasPiece", data.sensorTripped);
+
+        intakeAbsPos += data.intakeVelocityRadPerSec * 0.02;
     }
 
     // initiate changable measures for voltage, angle, and velocity
-    private final MutableMeasure<Voltage> voltage = mutable(Volts.of(0));
-    private final MutableMeasure<Angle> angle = mutable(Radians.of(0));
-    private final MutableMeasure<Velocity<Angle>> velocity = mutable(RadiansPerSecond.of(0)); 
+    private final MutableMeasure<Voltage> voltageMeasure = mutable(Volts.of(0));
+    private final MutableMeasure<Angle> angleMeasure = mutable(Radians.of(0));
+    private final MutableMeasure<Velocity<Angle>> velocityMeasure = mutable(RadiansPerSecond.of(0)); 
 
-    /*
-    SysIdRoutine routine = new SysIdRoutine(
+    private static double intakeAbsPos = 0;
+
+    SysIdRoutine intakeRoutine = new SysIdRoutine(
         new SysIdRoutine.Config(),
-        new SysIdRoutine.Mechanism(
+        new SysIdRoutine.Mechanism( // takes in voltage consumer, log consumer, and name of mechanism
+
             (Measure<Voltage> volts) -> {
                 setVoltage(volts.magnitude());},
+
             (log) -> {
-                log.motor()
+                log
+                .motor("intake-motor")
+                .voltage(
+                    voltageMeasure.mut_replace( 
+                        data.intakeVolts, // update mutable "voltage" with data
+                        Volts // the unit is volts
+                    )
+                ).angularPosition(
+                    angleMeasure.mut_replace(
+                        intakeAbsPos, // update mutable "angle" with data
+                        Radians // the unit is radians
+                    )
+                ).angularVelocity(
+                    velocityMeasure.mut_replace(
+                        data.intakeVelocityRadPerSec, // update mutable "angle" with data
+                        RadiansPerSecond // the unit is radians per second
+                    )
+                );
             },
+
             this
         )
-        );
-    */
+    );
+    
+    public Command getIntakeSysIDQuasistaticForwardTest() {
+        return intakeRoutine.quasistatic(SysIdRoutine.Direction.kForward);
+    }
+
+    public Command getIntakeSysIDQuasistaticReverseTest() {
+        return intakeRoutine.quasistatic(SysIdRoutine.Direction.kForward);
+    }
+
+    public Command getIntakeSysIDDynamicForwardTest() {
+        return intakeRoutine.dynamic(SysIdRoutine.Direction.kForward);
+    }
+
+    public Command getIntakeSysIDDynamicReverseTest() {
+        return intakeRoutine.dynamic(SysIdRoutine.Direction.kForward);
+    }
 }
