d084d3c1eb883bd6bcb5c64324c3570b9317e4c6
aadibhat09 <aadibhat09@gmail.com>
2024-02-19 16:12:43 -0800

    intake mutable measures

--- Team-3749-2024/src/main/java/frc/robot/subsystems/intake/Intake.java
+++ Team-3749-2024/src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,9 +1,14 @@
 package frc.robot.subsystems.intake;
 
+import static edu.wpi.first.units.MutableMeasure.mutable;
+import static edu.wpi.first.units.Units.*;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.units.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.Robot;
 import frc.robot.subsystems.intake.IntakeIO.IntakeData;
 import frc.robot.utils.Constants;
@@ -49,4 +54,22 @@ public class Intake extends SubsystemBase {
         SmartDashboard.putBoolean("intakeHasPiece", data.sensorTripped);
     }
 
+    // initiate changable measures for voltage, angle, and velocity
+    private final MutableMeasure<Voltage> voltage = mutable(Volts.of(0));
+    private final MutableMeasure<Angle> angle = mutable(Radians.of(0));
+    private final MutableMeasure<Velocity<Angle>> velocity = mutable(RadiansPerSecond.of(0)); 
+
+    /*
+    SysIdRoutine routine = new SysIdRoutine(
+        new SysIdRoutine.Config(),
+        new SysIdRoutine.Mechanism(
+            (Measure<Voltage> volts) -> {
+                setVoltage(volts.magnitude());},
+            (log) -> {
+                log.motor()
+            },
+            this
+        )
+        );
+    */
 }
