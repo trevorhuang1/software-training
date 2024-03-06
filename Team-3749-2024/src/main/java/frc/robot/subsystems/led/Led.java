626e88fcabb4552f58edc6b2088e17c961c8f8b4
NoahSimon8 <noah.simon9806@gmail.com>
2024-03-06 06:23:36 -0800

    proper led pattern import

--- Team-3749-2024/src/main/java/frc/robot/subsystems/led/Led.java
+++ Team-3749-2024/src/main/java/frc/robot/subsystems/led/Led.java
@@ -8,8 +8,7 @@ import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.led.LEDConstants.LEDConstants.LEDPattern;
-
+import frc.robot.subsystems.led.LEDConstants.LEDPattern;
 public class Led extends SubsystemBase {
 
     private AddressableLED LEDs = new AddressableLED(9); //port
