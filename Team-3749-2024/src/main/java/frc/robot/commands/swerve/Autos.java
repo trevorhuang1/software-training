e70d66ff20109db6d9d3d4d52f129429c37a50eb
SlushEE0 <101753528+SlushEE0@users.noreply.github.com>
2024-03-08 09:13:36 -0800

    feat: run choreo auto path

--- Team-3749-2024/src/main/java/frc/robot/commands/swerve/Autos.java
+++ Team-3749-2024/src/main/java/frc/robot/commands/swerve/Autos.java
@@ -1,7 +1,6 @@
 package frc.robot.commands.swerve;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -12,40 +11,56 @@ import frc.robot.Robot;
 
 public class Autos {
 
-    public static Command getStraightLine() {
-        return AutoUtils.getAutoPath("straight line", new Pose2d(4, 5, new Rotation2d()));
-    }
+  public static Command getStraightLine() {
+    return AutoUtils.getAutoPath(
+      "straight line",
+      new Pose2d(4, 5, new Rotation2d())
+    );
+  }
 
-    public static Command get4Piece() {
-        // return AutoUtils.getCycle(0);
-        return new SequentialCommandGroup(AutoUtils.getCycle(0),
-                AutoUtils.getAutoPath("4 piece middle solo", new Pose2d(1.32, 5.44, new Rotation2d())),
-                AutoUtils.getCycle(8));
+  public static Command get4Piece() {
+    // return AutoUtils.getCycle(0);
+    return new SequentialCommandGroup(
+      AutoUtils.getCycle(0),
+      AutoUtils.getAutoPath(
+        "4 piece middle solo",
+        new Pose2d(1.32, 5.44, new Rotation2d())
+      ),
+      AutoUtils.getCycle(8)
+    );
+    // TEMPLATE
+    // return new AutoUtils.getChoreoAutoPath("PATH NAME", new Pose2d(POSE));
 
-        // return new SequentialCommandGroup(AutoUtils.getCycle(0),
-        // AutoUtils.getAutoPath("4 piece middle right", new Pose2d(1.32, 5.44, new
-        // Rotation2d())),
-        // AutoUtils.getCycle(2), AutoUtils.getCycle(10));
+    // return new SequentialCommandGroup(AutoUtils.getCycle(0),
+    // AutoUtils.getAutoPath("4 piece middle right", new Pose2d(1.32, 5.44, new
+    // Rotation2d())),
+    // AutoUtils.getCycle(2), AutoUtils.getCycle(10));
 
-        // return new SequentialCommandGroup(AutoUtils.getCycle(0),
-        // AutoUtils.getAutoPath("4 piece middle wing", new Pose2d(1.32, 5.44, new
-        // Rotation2d())),
-        // AutoUtils.getCycle(2), AutoUtils.getCycle(6)), AutoUtils.getCycle(13);
+    // return new SequentialCommandGroup(AutoUtils.getCycle(0),
+    // AutoUtils.getAutoPath("4 piece middle wing", new Pose2d(1.32, 5.44, new
+    // Rotation2d())),
+    // AutoUtils.getCycle(2), AutoUtils.getCycle(6)), AutoUtils.getCycle(13);
 
-        // return new SequentialCommandGroup(AutoUtils.getCycle(0),
-        // AutoUtils.getAutoPath("4 piece middle", new Pose2d(1.32, 5.44, new
-        // Rotation2d())),
-        // AutoUtils.getCycle(2), AutoUtils.getCycle(6)), AutoUtils.getCycle(13);
-        
-        // return new SequentialCommandGroup(AutoUtils.getCycle(0),
-        // AutoUtils.getAutoPath("4 piece middle left", new Pose2d(1.32, 5.44, new
-        // Rotation2d())),
-        // AutoUtils.getCycle(2), AutoUtils.getCycle(10));
+    // return new SequentialCommandGroup(AutoUtils.getCycle(0),
+    // AutoUtils.getAutoPath("4 piece middle", new Pose2d(1.32, 5.44, new
+    // Rotation2d())),
+    // AutoUtils.getCycle(2), AutoUtils.getCycle(6)), AutoUtils.getCycle(13);
 
-    }
+    // return new SequentialCommandGroup(AutoUtils.getCycle(0),
+    // AutoUtils.getAutoPath("4 piece middle left", new Pose2d(1.32, 5.44, new
+    // Rotation2d())),
+    // AutoUtils.getCycle(2), AutoUtils.getCycle(10));
 
-    public static Command getTroll() {
-        return new SequentialCommandGroup(AutoUtils.getCycle(0), AutoUtils.getTroll(),
-                AutoUtils.getAutoPath("top-speaker-troll", new Pose2d(1.32, 5.44, new Rotation2d())));
-    }
+  }
+
+  public static Command getTroll() {
+    return new SequentialCommandGroup(
+      AutoUtils.getCycle(0),
+      AutoUtils.getTroll(),
+      AutoUtils.getAutoPath(
+        "top-speaker-troll",
+        new Pose2d(1.32, 5.44, new Rotation2d())
+      )
+    );
+  }
 }
