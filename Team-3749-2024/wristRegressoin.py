b25d058fca37c7b5cb0466acbd30ce9dbed4e777
rohin <rs.rohinsood@gmail.com>
2024-04-27 21:27:32 -0700

    clean up wrist regression

--- Team-3749-2024/wristRegressoin.py
+++ Team-3749-2024/wristRegressoin.py
@@ -12,10 +12,6 @@ x1 = data["4 Bar Position Deg"].values /180.0 * math.pi
 x2 = data["Arm Position Deg"].values/180.0 * math.pi
 y = data["Gravity Voltage"].values
 
-
-# if (x1.size != x2.size or x2.size!= y.size):c1
-#     throw Exception
-
 # Create design matrix
 X = np.column_stack((np.ones(x1.size),
                      x1,
@@ -40,7 +36,6 @@ predicted_y = results.predict(X);
 x1_str = 'x'
 x2_str = 'y'
 
-
 # Construct the equation string
 print(coefficients)
 
@@ -57,28 +52,11 @@ equation += f'{coefficients[9]} * {x1_str+x2_str+"^2"} + '
 equation += f'{coefficients[10]} * {x1_str+"^2"+x2_str+"^2"} '
 equation += f'{coefficients[11]} * {x1_str+"^3"+x2_str+"^2"} '
 
-
-# def predict(x1,x2):
-#     return coefficients[0] + coefficients[1] * x1 + coefficients[2] * x1**2 + coefficients[3] * x1**3 + coefficients[4] * x2**1 + coefficients[5] * x2**2
 rmse = mean_squared_error(y, predicted_y, squared=False)
 
 # Print or use the equation string
 print("Equation of the fitted quadratic regression model:")
 print(equation)
-# print(predicted_y)
-# print("Predicition: 15, 0")
-# print("0 " + str(predict(0,0)))
-# print(predict(15,0))
-# print(predict(30,0))
-# print("45 " + str(predict(45,0)))
-# print(predict(60,0))
-# print(predict(75,0))
-# print("90 "+ str(predict(90,0)))
-# print(predict(105,0))
-# print(predict(120,0))
-# print("135 " + str(predict(135,0)))
-# print(predict(150,0))
-
 
 print("R Squared")
 print(results.rsquared)
